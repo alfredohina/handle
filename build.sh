64048d95f50e2106ad236006896e8f97262340f1
Alfredo Hinarejos <alfredo.hina@gmail.com>
2019-02-23 19:16:39 +0100

    heroku3

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-rm -rf ./client/build
+rm -rf ./client/WebProject2/build
 rm -rf ./server/public/
-cd client
+cd client/WebProject2
 npm install
 npm run build
 cd ..
-cp -r ./client/build/ ./server/public
+cp -r ./client/WebProject2/build/ ./server/public
 echo "*" > ./server/public/.gitignore
 echo "Done build & copy!"
