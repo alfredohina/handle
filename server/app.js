2b0a16ff95c814602945d350e22abda100268ef6
Alfredo Hinarejos <alfredo.hina@gmail.com>
2019-02-26 19:54:31 +0100

    heroki10

--- server/app.js
+++ server/app.js
@@ -13,8 +13,7 @@ const MongoStore = require('connect-mongo')(session);
 const flash      = require("connect-flash");
     
 
-mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
+mongoose.connect(`${process.env.MONGO_URL}`, { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
